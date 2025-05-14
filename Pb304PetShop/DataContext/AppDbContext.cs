35df55adef13bcad84a8962670a6b281026e813f
Rafet <rafetr@risk.az>
2025-05-14 18:03:12 +0400

    simplified models

--- Pb304PetShop/DataContext/AppDbContext.cs
+++ Pb304PetShop/DataContext/AppDbContext.cs
@@ -12,14 +12,6 @@ namespace Pb304PetShop.DataContext
 
         public DbSet<Slider> Sliders { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductImage> ProductImages { get; set; }
-        public DbSet<ProductColor> ProductColors { get; set; }
-        public DbSet<ProductCategory> ProductCategories { get; set; }
-        public DbSet<ProductTag> ProductTags { get; set; }
-        public DbSet<Color> Colors { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-        public DbSet<StockByColor> StockByColors { get; set; }
-        public DbSet<StockBySize> StockBySizes { get; set; }
     }
 }
